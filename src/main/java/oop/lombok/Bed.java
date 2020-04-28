630a51225417ba87117ee06ccaf593a9ecc2d8ca
George <goggeliz@gmail.com>
2020-04-28 23:01:26 +0300

    delete comments

--- src/main/java/oop/lombok/Bed.java
+++ src/main/java/oop/lombok/Bed.java
@@ -11,25 +11,4 @@ public class Bed {
     private String treeFromWhichItIsMade;
     private String bedDimensions;
 
-//    public Bed(String treeFromWhichItIsMade, String bedDimensions) {
-//        this.treeFromWhichItIsMade = treeFromWhichItIsMade;
-//        this.bedDimensions = bedDimensions;
-//    }
-//
-//    public String getTreeFromWhichItIsMade() {
-//        return treeFromWhichItIsMade;
-//    }
-//
-//    public void setTreeFromWhichItIsMade(String treeFromWhichItIsMade) {
-//        this.treeFromWhichItIsMade = treeFromWhichItIsMade;
-//
-//    }
-//
-//    public String getBedDimensions() {
-//        return bedDimensions;
-//    }
-//
-//    public void setBedDimensions(String bedDimensions) {
-//        this.bedDimensions = bedDimensions;
-//        }
     }
